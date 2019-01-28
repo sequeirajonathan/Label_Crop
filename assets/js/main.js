6aa2becc0f9abb0ae8ed55d674268b1136a50732
Jonathan Sequeira <jsequeira@ryzeagency.com>
2019-01-27 22:11:05 -0500

    Front started/File name value processed successfuly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// const pdf = require('node-pdftk');
+
+let $fileName = $('input[name="pdf"]');
+let $uploadFile = $('#upload');
+let fileValue; 
+let fileName;
+
+$(document).ready(function () {
+    $uploadFile.click(function () {
+        $fileName.trigger('click');
+    });
+    $fileName.change(function () {
+        fileValue = this.value;
+        fileName = typeof value == 'string' ? fileValue.match(/[^\/\\]+$/)[0] : value[0];
+        console.log(fileName);
+    })
+});
